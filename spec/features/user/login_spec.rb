8562cf15bac26030f8e3bbffa14f7613fd54ddcc
Helio Albano de Oliveira <helioalb@gmail.com>
2019-02-28 09:16:03 -0300

    Add logout feature specs

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -4,6 +4,7 @@ RSpec.feature 'login', type: :feature do
   let(:user) { create(:user) }
 
   scenario 'user navigates to the login page and succesfully logs in', js: true do
+    user
     visit root_path
     find('nav a', text: 'Login').click
     fill_in 'user[email]', with: user.email
